73aff5627b22675f2132762ccd28c22aa6127234
ChristerNilsson <janchrister.nilsson@gmail.com>
2021-10-07 18:22:01 +0200

    Arbetsplatsbaserade resor

--- clean_rvu_1116_new.py
+++ clean_rvu_1116_new.py
@@ -5,6 +5,8 @@ import pandas as pd
 import json
 import math
 import numpy as np
+import pydash as _
+import operator
 
 UNKNOWN = -99
 
@@ -28,7 +30,7 @@ def runAsserts():
 	assert ModeRecoded('sparkcykel') == 'övrigt'
 
 	rows = [{'A':1, 'B':2, 'C':3}, {'A':4, 'B':5, 'C':6}]
-	assert pickColumns(['A','C'],rows) == [{'A':1, 'C':3}, {'A':4, 'C':6}]
+	#assert pickColumns(['A','C'],rows) == [{'A':1, 'C':3}, {'A':4, 'C':6}]
 	assert renameColumns({'A':'Adam','C':'Cesar'},rows) == [{'Adam':1, 'B':2, 'Cesar':3}, {'Adam':4, 'B':5, 'Cesar':6}]
 
 	assert mode_lookup[1] == 'gång'
@@ -138,6 +140,29 @@ def CreateDiary(trip_list):
 		row['dur'] = ConvertToMinutes(row['b_kl']) - ConvertToMinutes(row['a_kl'])
 	return activities
 
+def freq (arr):
+	d = {}
+	for item in arr:
+		if item not in d: d[item] = 0
+		d[item] += 1
+	return d
+
+def ToTimestep(minutes): return minutes // 30 # Gör om tid-från-midnatt till tidsperiod
+
+def RangeFromStartEnd(start, end):
+  ranges = []
+  s = ToTimestep(start)
+  e = 1 + ToTimestep(end)
+  if e <= s:
+    # Special case when activity crosses midnight
+    e_m = 1 + ToTimestep(24 * 60 - 1)
+    s_m = ToTimestep(24*60)
+    ranges.append(range(s, e_m))
+    ranges.append(range(s_m, e))
+  else:
+    ranges.append(range(s,e))
+  return ranges
+
 def groupBy(arr, cols):
 	result = {}
 	for row in arr:
@@ -192,7 +217,7 @@ def renameColumns(trans, rows):
 def replaceNA(rvu):
 	for row in rvu:
 		for key in row:
-			if key == 'VIKT':
+			if key == 'VIKT_DAG':
 				row[key] = round(row[key], 3)
 			else:
 				row[key] = UNKNOWN if math.isnan(row[key]) else round(row[key])
@@ -279,12 +304,229 @@ def TourProperties(tour_diary):
 			'split_act': split_act,
 		}
 
+def WB_TourProperties(tour_diary):
+	all_activities = [r for r in tour_diary if r['mode'] == 'aktivitet']
+	activities = [r for r in all_activities if r['purpose'] != 'bostad']
+	trips = [r for r in tour_diary if r['mode'] != 'aktivitet']
+
+	uenr = trips[0]['UENR']
+	tour_id = trips[0]['tour']
+	dag = trips[0]['DAG']
+	weight = trips[0]['weight']
+	zoneA = trips[0]['zoneA']
+	zoneB = trips[0]['zoneB']
+
+	result = [trip['mode'] for trip in trips]
+	mode = ModeHierarchy(result)
+
+	for trip in trips:
+		trip['mode'] = ModeRecoded(trip['mode'])
+
+	mainmode_trips = [r for r in trips if r['mode'] == mode]
+
+	# Kollar att turen har monotont stigande avresa/ankomst för alla resor
+	# is_mono = tour_diary[['start_time', 'end_time']].apply(lambda x: x.is_monotonic_increasing, axis=0)
+	# from_ok = is_mono['start_time']
+	# to_ok = is_mono['end_time']
+	# times_ok = from_ok and to_ok
+
+	#lista = [tour['a_kl'] for tour in tour_diary]
+	times_ok =              _.is_monotone([tour['a_kl'] for tour in tour_diary], operator.le)
+	times_ok = times_ok and _.is_monotone([tour['b_kl'] for tour in tour_diary], operator.le)
+	times_ok = True
+	#is_monotone([1, 1, 2, 3], operator.le)
+
+	reason = ""
+	if not times_ok:
+		print([tour['a_kl'] for tour in tour_diary])
+		print([tour['b_kl'] for tour in tour_diary])
+		reason = 'non-monotonic'
+
+	roundtrip = len(activities) == 0
+	if roundtrip:
+		reason = 'roundtrip'
+
+	work_trip = 'Arbete' in [activity['purpose'] for activity in activities]
+	business_trip = 'Tjänste' in [activity['purpose'] for activity in activities]
+
+	if ~(work_trip and business_trip):
+		reason = 'non-WB-business'
+
+	ok = times_ok and not roundtrip and work_trip and business_trip
+
+	if not ok:
+		# print(f'{uenr} not ok, from ok {from_ok}, to ok {to_ok}')
+		# print(tour_diary)
+
+		dur = sum([trip['dur'] for trip in trips])
+		return {
+			'UENR': uenr,
+			'tour': tour_id,
+			'DAG': dag,
+			'zoneA': zoneA,
+			'zoneB': zoneB,
+			'purpose': reason,
+			'mode': mode,
+			'weight': weight,
+			'Adur': dur,
+			'Mdur': dur,
+			'split_act': False,
+			# 'activity_range': [],
+			# 'trip_range': [],
+			# 'main_trip_range': [],
+			# 'outbound_range': [],
+			# 'inbound_range': []
+		}
+
+	# Kandidat att ha en arbetsplatsbaserad tjänsteresa
+	else:
+
+		# act_range = RangeFromStartEnd(trips[0]['a_kl'], trips[-1]['b_kl'])
+		# Vi vill veta om det finns flera 'Arbete'-aktiviteter
+		work_acts = [a for a in activities if a['purpose'] == 'Arbete']
+		split_act = len(work_acts) > 1
+
+		# Om vi har en uppdelad arbets-aktivitet behöver vi kolla
+		# om den har en tjänsteresa i mitten nånstans
+		if split_act:
+
+			# Plocka ut de resor och aktiviteter som är mitt i arbetsresan
+			acts = [a for a in activities if a['purpose'] == 'Tjänste' and a['a_kl'] >= work_acts[0]['a_kl'] and a['b_kl'] <= work_acts[-1]['b_kl']]
+			if len(acts) < 1:
+				return {
+					'UENR': uenr,
+					'tour': tour_id,
+					'DAG': dag,
+					'zoneA': zoneA,
+					'zoneB': zoneB,
+					'purpose': reason,
+					'mode': mode,
+					'weight': weight,
+					'Adur': 0,
+					'Mdur': 0,
+					'split_act': split_act,
+					# 'activity_range': [],
+					# 'trip_range': [],
+					# 'main_trip_range': [],
+					# 'outbound_range': [],
+					# 'inbound_range': []
+				}
+			if len(trips) < 1:
+				return {
+					'UENR': uenr,
+					'tour': tour_id,
+					'DAG': dag,
+					'zoneA': zoneA,
+					'zoneB': zoneB,
+					'purpose': 'non-WB-business',
+					'mode': mode,
+					'weight': weight,
+					'Adur': 0,
+					'Mdur': 0,
+					'split_act': split_act,
+					# 'activity_range': [],
+					# 'trip_range': [],
+					# 'main_trip_range': [],
+					# 'outbound_range': [],
+					# 'inbound_range': []
+				}
+
+			# filter_tripstart = trips['a_kl'] >= work_acts[0]['a_kl']
+			# filter_tripend = trips['b_kl'] >= work_acts[-1]['b_kl']
+			# wb_trips = [t for t in trips if t['a_kl'] >= work_acts[0]['a_kl'] and t['b_kl'] >= work_acts[-1]['b_kl']]
+
+			#wb_mainmode = trips.loc[trips['duration'].idxmax()]['mode']
+			wb_mainmode = max([trip for trip in trips], key=lambda r : r["dur"])["mode"]
+
+			#wb_longest_act = acts.loc[acts['duration'].idxmax()]
+			wb_longest_act = max([act for act in acts], key=lambda r : r["dur"])
+
+			#main_zone = [t for t in tour_diary if t['purpose'] != 'bostad']
+			#zone = max(main_zone, key=lambda r : r["dur"])
+
+			act_duration = sum([a['dur'] for a in acts])
+			mainmode_duration = sum([trip['dur'] for trip in trips if trip['mode'] == wb_mainmode])
+			act_range = RangeFromStartEnd(wb_longest_act['a_kl'], wb_longest_act['b_kl'])
+
+			if len(act_range) > 1:
+				pass
+				# s_act = act_range[0][0]
+				# e_act = act_range[-1][-1]
+			else:
+				# if len(act_range[0]) > 1:
+				# 	s_act = act_range[0][0]
+				# 	e_act = act_range[0][-1]
+				# elif isinstance(act_range[0], int):
+				# 	s_act = act_range
+				# 	e_act = s_act
+				# else:
+				# 	s_act = act_range[0][0]
+				# 	e_act = s_act
+
+				# outbound_range = []
+				# inbound_range = []
+				# trip_range = []
+				# wp_range = []
+				# for t in trips:
+				# 	ranges = RangeFromStartEnd(t['a_kl'], t['b_kl'])
+				# 	trip_range.extend(ranges)
+				# main_trip_range = []
+				# for mt in mainmode_trips:
+				# 	ranges = RangeFromStartEnd(mt['a_kl'], mt['b_kl'])
+				# 	main_trip_range.extend(ranges)
+				# 	s = ranges[0][0]
+				# 	if s <= s_act: outbound_range.extend(ranges)
+				# 	if s >= e_act: inbound_range.extend(ranges)
+
+				tour = {
+					'UENR': uenr,
+					'tour': tour_id,
+					'DAG': dag,
+					'zoneA': zoneA,
+					'zoneB': zoneB,
+					'purpose': 'Tjänste (AB)',
+					'mode': mode,
+					'weight': weight,
+					'Adur': act_duration,
+					'Mdur': mainmode_duration,
+					'split_act': split_act,
+					# 'activity_range': act_range,
+					# 'trip_range': trip_range,
+					# 'main_trip_range': main_trip_range,
+					# 'outbound_range': outbound_range,
+					# 'inbound_range': inbound_range
+
+				}
+		# Annars har den ingen arbetsplatsbaserad tur
+		else:
+			tour = {
+				'UENR': uenr,
+				'tour': tour_id,
+				'DAG': dag,
+				'zoneA': zoneA,
+				'zoneB': zoneB,
+				'purpose': 'non-WB-business',
+				'mode': mode,
+				'weight': weight,
+				'Adur': 0,
+				'Mdur': 0,
+				'split_act': split_act,
+				# 'activity_range': [],
+				# 'trip_range': [],
+				# 'main_trip_range': [],
+				# 'outbound_range': [],
+				# 'inbound_range': []
+			}
+
+	return tour
+
 with open('settings.json') as f: settings = json.load(f)
 root = settings['root']
 input = root + settings['input']
 output = root + settings['output']
 rvu_input = input + settings['rvu']
 tour_arb_output = output + "tour_arb.csv"
+tour_WB_output = output + "tour_WB.csv"
 koder = root + settings['koder']
 skiprows = settings['skiprows']
 nrows = settings['nrows']
@@ -293,7 +535,7 @@ dtype = {'id': np.int32}
 mode_codes = pd.read_csv(koder + "fm_kod.txt", sep='\t', dtype = dtype )
 mode_lookup = dict(zip(mode_codes["id"], mode_codes["grp"]))
 
-purpose_codes = pd.read_csv(koder + "ärende_kod_gen.txt", sep='\t')
+purpose_codes = pd.read_csv(koder + "ärende_kod_gen.txt", sep='\t', dtype = dtype)
 purpose_lookup = dict(zip(purpose_codes["id"], purpose_codes["grp"]))
 
 place_codes = pd.read_csv(koder + "plats_kod.txt", sep='\t')
@@ -307,10 +549,7 @@ work_lookup = dict(zip(work_codes["kod"], work_codes["status"]))
 
 runAsserts()
 
-cols = "Lopnr,UENR,AR,Manad,DAG,HNR,DNR,BOSAMS,S_KL,SP,S_SAMS,SV1,M_KL,MP,M_SAMS,SV2,HAR,DAR,FRD,KM,TID,H_ANT,H_ANT_DR,H_BARNN,H_BARNV,H_ENKEL,H_ENSAM,H_FORDON,H_PERS,D_PERS,FRD_agg,HAR_agg,DAR_agg,ID,Zon_a,Zon_b,KORKORT,Vikt,Noll,SP1_MP1".split(',')
-
-#cols = "UENR,BOST_LAN,D_ARE,D_FORD,D_A_KL,D_B_KL,UEDAG,VIKT_DAG,D_A_S,D_B_S,D_A_SVE,D_B_SVE,D_A_PKT,D_B_PKT".split(',')
-#cols = "UENR,HAR,FRD,S_KL,M_KL,DAG,Vikt,BOSAMS,S_SAMS,M_SAMS,SV1,SV2,SP,MP,HAR_agg".split(',')
+cols = "UENR,UEDAG,BOST_LAN,D_ARE,D_FORD,D_A_KL,D_B_KL,UEDAG,VIKT_DAG,D_A_S,D_B_S,D_A_SVE,D_B_SVE,D_A_PKT,D_B_PKT".split(',')
 rvuA = pd.read_csv(rvu_input, usecols=cols, nrows=nrows, skiprows=range(1,skiprows))
 rvuB = rvuA.to_dict('records')
 rvuB = replaceNA(rvuB)
@@ -320,27 +559,19 @@ rvuB = replaceNA(rvuB)
 # I RVU:erna är skola ett ärende, medan Sampers skiljer på skola för olika åldersgrupper. Här använder vi samma uppdelning som i skattningen, dvs i tre grupper: Grundskola för åldrarna 6-15 år, gymnasium för 16-18 år och vuxenutbildning för 19 år och uppåt.
 # Read and define lookup tables for survey codes
 
-rvuC = [r for r in rvuB if r['SV1'] == 1 and r['SV2'] == 1] # filtera bort utrikesresor
-
-#cols = 'UENR,UEDAG,VIKT_DAG,D_A_S,D_B_S,D_FORD,D_ARE,D_A_PKT,D_B_PKT,D_A_KL,D_B_KL,BOST_LAN'.split(',')
-#rvuD = pickColumns(cols, rvuC)
-rvuE = renameColumns({"S_KL":"A_KL", "M_KL":"B_KL", "SP":"A_P", "MP":"B_P", "S_SAMS":"A_SAMS", "M_SAMS":"B_SAMS", "Vikt":"VIKT"}, rvuC) # "DAG": "DAG", "FRD":"FRD",
+rvuC = [r for r in rvuB if r['D_A_SVE'] == 1 and r['D_A_SVE'] == 1] # filtera bort utrikesresor
+rvuE = renameColumns({"D_A_KL":"A_KL", "D_B_KL":"B_KL", "D_A_PKT":"A_P", "D_B_PKT":"B_P", "D_ARE":"ARE", "D_FORD":"FRD", "BOST_LAN":"LAN", "UEDAG": "DAG", "D_A_S":"A_SAMS", "D_B_S":"B_SAMS","VIKT_DAG":"VIKT"}, rvuC)
 rvuF = [r for r in rvuE if r['DAG'] <= 7] # filtrera fram veckodagar.
 rvuG = [r for r in rvuF if not (r['A_P'] == 1 and r['B_P'] == 1 or r['A_P'] == 2 and r['B_P'] == 2 or r['A_P'] == 3 and r['B_P'] == 3)] # filtrera bort rundresor
 
 for row in rvuG:
-	#print(row['UENR'])
 	row['mode'] = mode_lookup[row['FRD']]
-
-	#row["purpose"] = row["HAR"] #purpose_lookup[row["ARE"]]
-	row["purpose"] = row["HAR_agg"] #purpose_lookup[row["ARE"]]
-	#row["purpose"] = purpose_lookup[row["ARE"]]
-
+	row["purpose"] = purpose_lookup[row["ARE"]]
 	row["a_p"] = place_lookup[row["A_P"]]
 	row["b_p"] = place_lookup[row["B_P"]]
 	row['a_kl'] = row["A_KL"]
 	row['b_kl'] = row["B_KL"]
-	row['LAN'] = row['BOSAMS']//1000000
+	row['weight'] = row["VIKT"]
 	if row["LAN"] == -1:
 		row["region"] = -1
 	else:
@@ -358,13 +589,21 @@ for group in rvuH:
 	for item in lst:
 		rvuI.append(item)
 
+cols = 'UENR,DAG,tour,purpose,mode,weight,Adur,Mdur,zoneA,zoneB,parts'.split(',')
 rvuJ = [r for r in rvuI if r['day_type'] in ['bostad->bostad','bostad->annat','bostad->bostad_ovr','bostad->bostad_fri']]  # homebased_diaries
 rvuK = groupBy(rvuJ, ['UENR', 'tour'])
 rvuL = [TourProperties(rvuK[group]) for group in rvuK]
-
-cols = 'UENR,DAG,tour,purpose,mode,weight,Adur,Mdur,zoneA,zoneB,parts'.split(',')
 rvuM = pickColumns(cols, rvuL)
-
 if len(rvuM) > 0:
 	ttdf_arb = pd.DataFrame.from_dict(rvuM)
 	ttdf_arb.to_csv(tour_arb_output, index=False, columns=cols)
+
+#cols = 'UENR,DAG,tour,purpose,mode,weight,Adur,Mdur,zoneA,zoneB,activity_range,trip_range,main_trip_range,outbound_range,inbound_range'.split(',')
+cols = 'UENR,DAG,tour,purpose,mode,weight,Adur,Mdur,zoneA,zoneB'.split(',')
+rvuJ = [r for r in rvuI if r['day_type'] in ['arbete->arbete', 'arbete->bostad', 'arbete->annat', 'arbete->bostad_fri', 'arbete->bostad_ovr']]  # Workbased
+rvuK = groupBy(rvuJ, ['UENR', 'tour'])
+rvuL = [WB_TourProperties(rvuK[group]) for group in rvuK]
+rvuM = pickColumns(cols, rvuL)
+if len(rvuM) > 0:
+	ttdf_arb = pd.DataFrame.from_dict(rvuM)
+	ttdf_arb.to_csv(tour_WB_output, index=False, columns=cols)
